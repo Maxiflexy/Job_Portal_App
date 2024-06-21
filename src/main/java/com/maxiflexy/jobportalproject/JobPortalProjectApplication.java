88fad09c4c8d3d090af556836b9ed2382e7fe730
Maxiflexy <onyemax247@gmail.com>
2024-06-21 21:24:46 +0100

    initial commit

---
+++ src/main/java/com/maxiflexy/jobportalproject/JobPortalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maxiflexy.jobportalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxiflexy/jobportalproject/JobPortalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiflexy.jobportalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
